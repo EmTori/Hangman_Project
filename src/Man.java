cfcb2b72486592c95e3313fd5fa1146c61d380f0
EmTori <emily.houlihan@outlook.com>
2021-08-03 14:54:19 -0230

    Updated Hangman image

--- src/Man.java
+++ src/Man.java
@@ -1,94 +1,112 @@
 public class Man {
     public static void main(String[] args) {
-        System.out.println("Wrong guess 0: (Game Start)");
-        System.out.println("   +------+");
-        System.out.println("   |");
-        System.out.println("   |");
-        System.out.println("   |");
-        System.out.println("   |");
-        System.out.println("   |");
-        System.out.println("  _|_");
-        System.out.println("=/=|=\\=========");
+        //Beginning drawing, Start Game.
+        System.out.println("Welcome to Hangman");
+        System.out.println("——————————————————");
         System.out.println();
-        System.out.println("Wrong guess 1:");
-        System.out.println("   +------+");
-        System.out.println("   |      |");
-        System.out.println("   |");
-        System.out.println("   |");
-        System.out.println("   |");
-        System.out.println("   |");
-        System.out.println("  _|_");
-        System.out.println("=/=|=\\=========");
+        System.out.println("     +------+");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("    _|_");
+        System.out.println(" ==/=│=\\========");
+
+        //Wrong guess #1
+        System.out.println("Wrong guess, please guess a new letter.");
         System.out.println();
-        System.out.println("Wrong guess 2:");
-        System.out.println("   +------+");
-        System.out.println("   |      |");
-        System.out.println("   |     (ӧ)");
-        System.out.println("   |");
-        System.out.println("   |");
-        System.out.println("   |");
-        System.out.println("  _|_");
-        System.out.println("=/=|=\\=========");
+        System.out.println("     +------+");
+        System.out.println("     |      |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("    _|_");
+        System.out.println(" ==/=│=\\========");
+
+        //Wrong guess #2
+        System.out.println("Wrong guess, please guess a new letter.");
         System.out.println();
-        System.out.println("Wrong guess 3:");
-        System.out.println("   +------+");
-        System.out.println("   |      |");
-        System.out.println("   |     (ӧ)");
-        System.out.println("   |      |");
-        System.out.println("   |");
-        System.out.println("   |");
-        System.out.println("  _|_");
-        System.out.println("=/=|=\\=========");
+        System.out.println("     +------+");
+        System.out.println("     |      |");
+        System.out.println("     |     (Ӧ)");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("    _|_");
+        System.out.println(" ==/=│=\\========");
+
+        //Wrong guess #3
+        System.out.println("Wrong guess, please guess a new letter.");
         System.out.println();
-        System.out.println("Wrong guess 4:");
-        System.out.println("   +------+");
-        System.out.println("   |      |");
-        System.out.println("   |     (ӧ)");
-        System.out.println("   |     /|");
-        System.out.println("   |");
-        System.out.println("   |");
-        System.out.println("  _|_");
-        System.out.println("=/=|=\\=========");
+        System.out.println("     +------+");
+        System.out.println("     |      |");
+        System.out.println("     |     (Ӧ)");
+        System.out.println("     |      |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("    _|_");
+        System.out.println(" ==/=│=\\========");
+
+        //Wrong guess #4
+        System.out.println("Wrong guess, please guess a new letter.");
         System.out.println();
-        System.out.println("Wrong guess 5:");
-        System.out.println("   +------+");
-        System.out.println("   |      |");
-        System.out.println("   |     (Ӧ)");
-        System.out.println("   |     /|\\");
-        System.out.println("   |");
-        System.out.println("   |");
-        System.out.println("  _|_");
-        System.out.println("=/=|=\\=========");
+        System.out.println("     +------+");
+        System.out.println("     |      |");
+        System.out.println("     |     (Ӧ)");
+        System.out.println("     |     /|");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("    _|_");
+        System.out.println(" ==/=│=\\========");
+
+        //Wrong guess #5
+        System.out.println("Wrong guess, please guess a new letter.");
         System.out.println();
-        System.out.println("Wrong guess 6:");
-        System.out.println("   +------+");
-        System.out.println("   |      |");
-        System.out.println("   |     (Ӧ)");
-        System.out.println("   |     /|\\");
-        System.out.println("   |      |");
-        System.out.println("   |");
-        System.out.println("  _|_");
-        System.out.println("=/=|=\\=========");
+        System.out.println("     +------+");
+        System.out.println("     |      |");
+        System.out.println("     |     (Ӧ)");
+        System.out.println("     |     /|\\");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("    _|_");
+        System.out.println(" ==/=│=\\========");
+
+        //Wrong guess #6
+        System.out.println("Wrong guess, please guess a new letter.");
         System.out.println();
-        System.out.println("Wrong guess 7:");
-        System.out.println("   +------+");
-        System.out.println("   |      |");
-        System.out.println("   |     (Ӧ)");
-        System.out.println("   |     /|\\");
-        System.out.println("   |      |");
-        System.out.println("   |     /");
-        System.out.println("  _|_");
-        System.out.println("=/=|=\\=========");
+        System.out.println("     +------+");
+        System.out.println("     |      |");
+        System.out.println("     |     (Ӧ)");
+        System.out.println("     |     /|\\");
+        System.out.println("     |      │");
+        System.out.println("     |");
+        System.out.println("    _|_");
+        System.out.println(" ==/=│=\\========");
+
+        //Wrong guess #7
+        System.out.println("Wrong guess, please guess a new letter.");
         System.out.println();
-        System.out.println("Wrong guess 8: (Game Over)");
-        System.out.println("   +------+");
-        System.out.println("   |      |");
-        System.out.println("   |     (X)");
-        System.out.println("   |     /|\\");
-        System.out.println("   |      |");
-        System.out.println("   |     / \\");
-        System.out.println("  _|_");
-        System.out.println("=/=|=\\=========");
+        System.out.println("     +------+");
+        System.out.println("     |      |");
+        System.out.println("     |     (Ӧ)");
+        System.out.println("     |     /|\\");
+        System.out.println("     |      |");
+        System.out.println("     |     /");
+        System.out.println("    _|_");
+        System.out.println(" ==/=│=\\========");
+
+        //Wrong guess #8, Game over.
+        System.out.println("Wrong guess.");
         System.out.println();
+        System.out.println("     +------+");
+        System.out.println("     |      |");
+        System.out.println("     |     (X)             GAME OVER  ");
+        System.out.println("     |     /|\\            ———————————");
+        System.out.println("     |      |");
+        System.out.println("     |     / \\");
+        System.out.println("    _|_");
+        System.out.println(" ==/=│=\\========");
     }
 }
